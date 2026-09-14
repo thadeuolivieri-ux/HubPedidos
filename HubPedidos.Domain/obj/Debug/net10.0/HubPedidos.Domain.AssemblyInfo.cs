a90b0f1b7c3aab842ee2a8d7f831985a6d3daa34
thadeuolivieri-ux <thadeu.olivieri@gmail.com>
2026-09-14 14:57:40 +0000

    feat: finaliza unidade 3 com mapeamentos ef core e testes

--- HubPedidos.Domain/obj/Debug/net10.0/HubPedidos.Domain.AssemblyInfo.cs
+++ HubPedidos.Domain/obj/Debug/net10.0/HubPedidos.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HubPedidos.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f3b77161a1916edb6c03527b3b0aea66340dd0d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1908b5e919fd831c6599efcc05d2c9a41bc1ab9")]
 [assembly: System.Reflection.AssemblyProductAttribute("HubPedidos.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HubPedidos.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
